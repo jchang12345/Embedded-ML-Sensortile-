98e1f126469cf2fe3170e3b5e68b77fd335acc5b
derek <derekqxu@gmail.com>
2019-05-12 15:29:40 -0700

    minor changes

--- gen_model.py
+++ gen_model.py
@@ -30,198 +30,204 @@ def shuffle_data(X, y):
     y[indices] = y[shuffled_indices]
     return X, y
 
-# load the data from .csv
-X_raw_1 = np.genfromtxt('data/acc_data_1.csv', delimiter=',')
-X_raw_2 = np.genfromtxt('data/acc_data_2.csv', delimiter=',')
+def gen_model():
+    # load the data from .csv
+    X_raw_1 = np.genfromtxt('data/acc_data_1.csv', delimiter=',')
+    X_raw_2 = np.genfromtxt('data/acc_data_2.csv', delimiter=',')
 
-# look at the shapes of X_raw
-print(X_raw_1.shape)
-print(X_raw_2.shape)
+    # look at the shapes of X_raw
+    print('Raw Shape:')
+    print(X_raw_1.shape)
+    print(X_raw_2.shape)
 
-# split the data
-window_size = 30
-train_val_split = int(2/3*len(X_raw_1))
-X_train_1, y_train_1 = preprocess(X_raw_1[:train_val_split], window_size, 0)
-X_val_1, y_val_1 = preprocess(X_raw_1[train_val_split:], window_size, 0)
+    # split the data
+    window_size = 30
+    train_val_split = int(2/3*len(X_raw_1))
+    X_train_1, y_train_1 = preprocess(X_raw_1[:train_val_split], window_size, 0)
+    X_val_1, y_val_1 = preprocess(X_raw_1[train_val_split:], window_size, 0)
 
 
-train_val_split = int(2/3*len(X_raw_2))
-X_train_2, y_train_2 = preprocess(X_raw_2[:train_val_split], window_size, 1)
-X_val_2, y_val_2 = preprocess(X_raw_2[train_val_split:], window_size, 1)
+    train_val_split = int(2/3*len(X_raw_2))
+    X_train_2, y_train_2 = preprocess(X_raw_2[:train_val_split], window_size, 1)
+    X_val_2, y_val_2 = preprocess(X_raw_2[train_val_split:], window_size, 1)
 
-print('Data Transformation 1')
-print(X_train_1.shape)
-print(y_train_1.shape)
-print(X_train_2.shape)
-print(y_train_2.shape)
-print(X_val_1.shape)
-print(y_val_1.shape)
-print(X_val_2.shape)
-print(y_val_2.shape)
+    print('Data Transformation:')
+    print(X_train_1.shape)
+    print(y_train_1.shape)
+    print(X_train_2.shape)
+    print(y_train_2.shape)
+    print(X_val_1.shape)
+    print(y_val_1.shape)
+    print(X_val_2.shape)
+    print(y_val_2.shape)
 
-X_train = np.vstack([X_train_1, X_train_2])
-y_train = np.hstack([y_train_1, y_train_2])
-X_val = np.vstack([X_val_1, X_val_2])
-y_val = np.hstack([y_val_1, y_val_2])
+    X_train = np.vstack([X_train_1, X_train_2])
+    y_train = np.hstack([y_train_1, y_train_2])
+    X_val = np.vstack([X_val_1, X_val_2])
+    y_val = np.hstack([y_val_1, y_val_2])
 
-print('Data Transformation 2')
-print(X_train.shape)
-print(X_val.shape)
-print(y_train.shape)
-print(y_val.shape)
+    print('Data Transformation:')
+    print(X_train.shape)
+    print(X_val.shape)
+    print(y_train.shape)
+    print(y_val.shape)
 
-X_train, y_train = shuffle_data(X_train, y_train)
-X_val, y_val = shuffle_data(X_val, y_val)
+    X_train, y_train = shuffle_data(X_train, y_train)
+    X_val, y_val = shuffle_data(X_val, y_val)
 
-print('Data Transformation 3')
-print(X_train.shape)
-print(X_val.shape)
-print(to_categorical(y_train).shape)
-print(to_categorical(y_val).shape)
+    print('Data Transformation:')
+    print(X_train.shape)
+    print(X_val.shape)
+    print(to_categorical(y_train).shape)
+    print(to_categorical(y_val).shape)
 
-#import matplotlib.pyplot as plt
-#b_l,a_l = signal.butter(8, 0.01, 'low')
-b_h,a_h = signal.butter(8, 0.1, 'high')
-for i, sample in enumerate(X_train):
-    for j in range(sample.shape[-1]):
-        #temp = signal.filtfilt(b_l,a_l,sample[:,j])
-        X_train[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
-for i, sample in enumerate(X_val):
-    for j in range(sample.shape[-1]):
-        #temp = signal.filtfilt(b_l,a_l,sample[:,j])
-        X_val[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
+    #import matplotlib.pyplot as plt
+    #b_l,a_l = signal.butter(8, 0.01, 'low')
+    b_h,a_h = signal.butter(8, 0.1, 'high')
+    for i, sample in enumerate(X_train):
+        for j in range(sample.shape[-1]):
+            #temp = signal.filtfilt(b_l,a_l,sample[:,j])
+            X_train[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
+    for i, sample in enumerate(X_val):
+        for j in range(sample.shape[-1]):
+            #temp = signal.filtfilt(b_l,a_l,sample[:,j])
+            X_val[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
 
-print('Data Transformation 4')
-print(X_train.shape)
-print(X_val.shape)
-print(to_categorical(y_train).shape)
-print(to_categorical(y_val).shape)
+    print('Data Transformation:')
+    print(X_train.shape)
+    print(X_val.shape)
+    print(to_categorical(y_train).shape)
+    print(to_categorical(y_val).shape)
 
-# define tensorflow model
-reg = 1e-1
-model = tf.keras.Sequential([ \
-    Conv1D(16, kernel_size = 3, \
-        strides=1, activation = 'relu', \
-        input_shape=(window_size, 3)), \
-    Flatten(), \
-    Dense(8, activation='relu', \
+    # define tensorflow model
+    reg = 1e-3
+    model = tf.keras.Sequential([ \
+        Conv1D(16, kernel_size = 5, \
+            strides=1, activation = 'relu', \
+            kernel_regularizer = l2(reg), \
+            bias_regularizer = l2(reg), \
+            input_shape=(window_size, 3)), \
+        Flatten(), \
+        Dense(8, activation='relu', \
+              kernel_regularizer=l2(reg), \
+              bias_regularizer=l2(reg)), \
+        Dense(4, activation='relu', \
+              kernel_regularizer=l2(reg), \
+              bias_regularizer=l2(reg)), \
+        Dense(2, activation='softmax', \
+              kernel_regularizer=l2(reg), \
+              bias_regularizer=l2(reg)) \
+    ])
+    '''
+    Pure MLP:
+    Flatten(input_shape=(window_size, 3)), \
+    Dense(128, activation='relu', \
           kernel_regularizer=l2(reg), \
           bias_regularizer=l2(reg)), \
-    Dense(4, activation='relu', \
+    Dense(64, activation='relu', \
           kernel_regularizer=l2(reg), \
           bias_regularizer=l2(reg)), \
     Dense(2, activation='softmax', \
           kernel_regularizer=l2(reg), \
           bias_regularizer=l2(reg)) \
-])
-'''
-Pure MLP:
-Flatten(input_shape=(window_size, 3)), \
-Dense(128, activation='relu', \
-      kernel_regularizer=l2(reg), \
-      bias_regularizer=l2(reg)), \
-Dense(64, activation='relu', \
-      kernel_regularizer=l2(reg), \
-      bias_regularizer=l2(reg)), \
-Dense(2, activation='softmax', \
-      kernel_regularizer=l2(reg), \
-      bias_regularizer=l2(reg)) \
-'''
+    '''
 
-# define optimizer
-# optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-optimizer = Adam()
-model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-model.summary()
-model.save('model.h5')
+    # define optimizer
+    # optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    optimizer = Adam()
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+    model.save('model.h5')
 
-# train our model
-epochs = 10
-batch_size = 256
-history = model.fit(X_train, \
-		to_categorical(y_train), \
-                epochs=epochs, \
-                batch_size=batch_size,
-                validation_data = (X_val, to_categorical(y_val)))
+    # train our model
+    epochs = 10
+    batch_size = 256
+    history = model.fit(X_train, \
+                    to_categorical(y_train), \
+                    epochs=epochs, \
+                    batch_size=batch_size,
+                    validation_data = (X_val, to_categorical(y_val)))
 
-# Plot loss and accuracy
-plt.figure()
-plt.plot(range(epochs), history.history['loss'])
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Loss Curve')
-plt.show()
+    # Plot loss and accuracy
+    plt.figure()
+    plt.plot(range(epochs), history.history['loss'])
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss Curve')
+    plt.show()
 
-###################################################################
-import collections
-print('========================================')
-X_test_1 = X_train_1
-X_test_2 = X_train_2
-for i, sample in enumerate(X_test_1):
-    for j in range(sample.shape[-1]):
-        X_test_1[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
-for i, sample in enumerate(X_test_2):
-    for j in range(sample.shape[-1]):
-        X_test_2[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
+    print('Sanity Check:')
 
-results = model.predict(X_test_1, batch_size=256)
+    ###################################################################
+    import collections
+    print('========================================')
+    X_test_1 = X_train_1
+    X_test_2 = X_train_2
+    for i, sample in enumerate(X_test_1):
+        for j in range(sample.shape[-1]):
+            X_test_1[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
+    for i, sample in enumerate(X_test_2):
+        for j in range(sample.shape[-1]):
+            X_test_2[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
 
-classes = np.argmax(results, axis=1)
-temp = collections.Counter(classes)
+    results = model.predict(X_test_1, batch_size=256)
 
-chance_0 = temp[0]/len(classes)
-chance_1 = temp[1]/len(classes)
+    classes = np.argmax(results, axis=1)
+    temp = collections.Counter(classes)
 
-print('Xtrain_1')
-print('probability of class 0: {}'.format(chance_0))
-print('probability of class 1: {}'.format(chance_1))
-#print(y_train_1)
+    chance_0 = temp[0]/len(classes)
+    chance_1 = temp[1]/len(classes)
 
-results = model.predict(X_test_2, batch_size=256)
+    print('Xtrain_1')
+    print('probability of class 0: {}'.format(chance_0))
+    print('probability of class 1: {}'.format(chance_1))
+    #print(y_train_1)
 
-classes = np.argmax(results, axis=1)
-temp = collections.Counter(classes)
+    results = model.predict(X_test_2, batch_size=256)
 
-chance_0 = temp[0]/len(classes)
-chance_1 = temp[1]/len(classes)
+    classes = np.argmax(results, axis=1)
+    temp = collections.Counter(classes)
 
-print('Xtrain_2')
-print('probability of class 0: {}'.format(chance_0))
-print('probability of class 1: {}'.format(chance_1))
-#print(y_train_2)
-###################################################################
-print('========================================')
-X_test_1 = X_val_1
-X_test_2 = X_val_2
-for i, sample in enumerate(X_test_1):
-    for j in range(sample.shape[-1]):
-        X_test_1[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
-for i, sample in enumerate(X_test_2):
-    for j in range(sample.shape[-1]):
-        X_test_2[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
+    chance_0 = temp[0]/len(classes)
+    chance_1 = temp[1]/len(classes)
 
-results = model.predict(X_test_1, batch_size=256)
+    print('Xtrain_2')
+    print('probability of class 0: {}'.format(chance_0))
+    print('probability of class 1: {}'.format(chance_1))
+    #print(y_train_2)
+    ###################################################################
+    print('========================================')
+    X_test_1 = X_val_1
+    X_test_2 = X_val_2
+    for i, sample in enumerate(X_test_1):
+        for j in range(sample.shape[-1]):
+            X_test_1[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
+    for i, sample in enumerate(X_test_2):
+        for j in range(sample.shape[-1]):
+            X_test_2[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
 
-classes = np.argmax(results, axis=1)
-temp = collections.Counter(classes)
+    results = model.predict(X_test_1, batch_size=256)
 
-chance_0 = temp[0]/len(classes)
-chance_1 = temp[1]/len(classes)
+    classes = np.argmax(results, axis=1)
+    temp = collections.Counter(classes)
 
-print('Xval_1')
-print('probability of class 0: {}'.format(chance_0))
-print('probability of class 1: {}'.format(chance_1))
-#print(y_train_1)
+    chance_0 = temp[0]/len(classes)
+    chance_1 = temp[1]/len(classes)
 
-results = model.predict(X_test_2, batch_size=256)
+    print('Xval_1')
+    print('probability of class 0: {}'.format(chance_0))
+    print('probability of class 1: {}'.format(chance_1))
+    #print(y_train_1)
 
-classes = np.argmax(results, axis=1)
-temp = collections.Counter(classes)
+    results = model.predict(X_test_2, batch_size=256)
 
-chance_0 = temp[0]/len(classes)
-chance_1 = temp[1]/len(classes)
+    classes = np.argmax(results, axis=1)
+    temp = collections.Counter(classes)
 
-print('Xval_2')
-print('probability of class 0: {}'.format(chance_0))
-print('probability of class 1: {}'.format(chance_1))
-#print(y_train_2)
+    chance_0 = temp[0]/len(classes)
+    chance_1 = temp[1]/len(classes)
+
+    print('Xval_2')
+    print('probability of class 0: {}'.format(chance_0))
+    print('probability of class 1: {}'.format(chance_1))
+    #print(y_train_2)
--- run_model.py
+++ run_model.py
@@ -32,52 +32,53 @@ def shuffle_data(X, y):
     y[indices] = y[shuffled_indices]
     return X, y
 
-# load the data from .csv
-X_raw_1 = np.genfromtxt('data/acc_data_1_test.csv', delimiter=',')
-X_raw_2 = np.genfromtxt('data/acc_data_2_test.csv', delimiter=',')
+def run_model():
+    # load the data from .csv
+    X_raw_1 = np.genfromtxt('data/acc_data_1_test.csv', delimiter=',')
+    X_raw_2 = np.genfromtxt('data/acc_data_2_test.csv', delimiter=',')
 
-# look at the shapes of X_raw
-print(X_raw_1.shape)
-print(X_raw_2.shape)
+    # look at the shapes of X_raw
+    print(X_raw_1.shape)
+    print(X_raw_2.shape)
 
-window_size = 30
-X_1, y_1 = preprocess(X_raw_1, window_size, 0)
-X_2, y_2 = preprocess(X_raw_2, window_size, 1)
+    window_size = 30
+    X_1, y_1 = preprocess(X_raw_1, window_size, 0)
+    X_2, y_2 = preprocess(X_raw_2, window_size, 1)
 
-#import matplotlib.pyplot as plt
-#b_l,a_l = signal.butter(8, 0.01, 'low')
-b_h,a_h = signal.butter(8, 0.1, 'high')
-for i, sample in enumerate(X_1):
-    for j in range(sample.shape[-1]):
-        #temp = signal.filtfilt(b_l,a_l,sample[:,j])
-        X_1[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
-for i, sample in enumerate(X_2):
-    for j in range(sample.shape[-1]):
-        #temp = signal.filtfilt(b_l,a_l,sample[:,j])
-        X_2[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
+    #import matplotlib.pyplot as plt
+    #b_l,a_l = signal.butter(8, 0.01, 'low')
+    b_h,a_h = signal.butter(8, 0.1, 'high')
+    for i, sample in enumerate(X_1):
+        for j in range(sample.shape[-1]):
+            #temp = signal.filtfilt(b_l,a_l,sample[:,j])
+            X_1[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
+    for i, sample in enumerate(X_2):
+        for j in range(sample.shape[-1]):
+            #temp = signal.filtfilt(b_l,a_l,sample[:,j])
+            X_2[i,:,j] = signal.filtfilt(b_h,a_h,sample[:,j])
 
-# load tensorflow model
-model = load_model('model.h5')
-results = model.predict(X_1, batch_size=256)
+    # load tensorflow model
+    model = load_model('model.h5')
+    results = model.predict(X_1, batch_size=256)
 
-classes = np.argmax(results, axis=1)
-temp = collections.Counter(classes)
+    classes = np.argmax(results, axis=1)
+    temp = collections.Counter(classes)
 
-chance_0 = temp[0]/len(classes)
-chance_1 = temp[1]/len(classes)
+    chance_0 = temp[0]/len(classes)
+    chance_1 = temp[1]/len(classes)
 
-print('class 1')
-print('probability of class 0: {}'.format(chance_0))
-print('probability of class 1: {}'.format(chance_1))
+    print('class 1')
+    print('probability of class 0: {}'.format(chance_0))
+    print('probability of class 1: {}'.format(chance_1))
 
-results = model.predict(X_2, batch_size=256)
+    results = model.predict(X_2, batch_size=256)
 
-classes = np.argmax(results, axis=1)
-temp = collections.Counter(classes)
+    classes = np.argmax(results, axis=1)
+    temp = collections.Counter(classes)
 
-chance_0 = temp[0]/len(classes)
-chance_1 = temp[1]/len(classes)
+    chance_0 = temp[0]/len(classes)
+    chance_1 = temp[1]/len(classes)
 
-print('class 2')
-print('probability of class 0: {}'.format(chance_0))
-print('probability of class 1: {}'.format(chance_1))
+    print('class 2')
+    print('probability of class 0: {}'.format(chance_0))
+    print('probability of class 1: {}'.format(chance_1))
